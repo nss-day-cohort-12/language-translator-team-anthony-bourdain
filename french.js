93cd4fb926d64a8fcbb35cbb89b1b3f0e0aed8b5
shupac800 <davidshuman@gmail.com>
2016-01-27 16:10:47 -0600

    added multiple-word functionality and unknown-word checking to french.js and turkish.js; made lexicon object into PRIVATE variable

--- french.js
+++ french.js
@@ -3,16 +3,25 @@
 // group project
 
 var Translate = (function(oldTranslate) {
-
-  // create new k:v pair in oldTranslate
-  oldTranslate.french = function(inputText) {
-    var lexicon = { merry: "joyeaux",
+  var lexicon =   { merry: "joyeaux",
                     christmas: "Noel",
                     and: "et",
                     happy: "bonne",
                     new: "nouvelle",
                     year: "annee" };
-    return lexicon[inputText];
+
+  // create new k:v pair in oldTranslate
+  oldTranslate.french = function(inputText) {
+    inputTextArray = inputText.split(" ");  // convert string into array of individual words
+    var outputString = "";  // initialize blank string
+    inputTextArray.forEach(function(currentWord) {
+      if (lexicon[currentWord]) {  // is currentWord in the lexicon?
+        outputString += lexicon[currentWord] + " ";  // if so, add translation of currentWord to outputString
+      } else {
+        outputString += "??? ";  // if not, add "???" to outputString
+      }
+    });
+    return outputString;
   };
 
   return oldTranslate;  // return new object
--- turkish.js
+++ turkish.js
@@ -1,18 +1,27 @@
-// module to augment Translate class with additional method
+// module to augment allLogic class with additional method
 // 27 Jan 2016
 // group project
 
 var Translate = (function(oldTranslate) {
-
-  // create new k:v pair in oldTranslate
-  oldTranslate.turkish = function(inputText) {
-    var lexicon = { merry: "mutlu",
+  var lexicon =   { merry: "mutlu",
                     christmas: "Noeller",
                     and: "ve",
                     happy: "mutlu",
                     new: "yeni",
                     year: "yillar" };
-    return lexicon[inputText];
+
+  // create new k:v pair in oldTranslate
+  oldTranslate.turkish = function(inputText) {
+    inputTextArray = inputText.split(" ");  // convert string into array of individual words
+    var outputString = "";  // initialize blank string
+    inputTextArray.forEach(function(currentWord) {
+      if (lexicon[currentWord]) {  // is currentWord in the lexicon?
+        outputString += lexicon[currentWord] + " ";  // if so, add translation of currentWord to outputString
+      } else {
+        outputString += "??? ";  // if not, add "???" to outputString
+      }
+    });
+    return outputString;
   };
 
   return oldTranslate;  // return new object
