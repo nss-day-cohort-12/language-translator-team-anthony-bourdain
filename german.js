17188eb66306fb76eb835d9df541424f9d175ebf
shupac800 <davidshuman@gmail.com>
2016-01-28 11:03:44 -0600

    everything working; fixed german.js; added comments to interactive.js

--- german.js
+++ german.js
@@ -1,13 +1,23 @@
 var Translate = (function (oldTranslate) {
-		var lexicon = {
-			merry: "fröhlich",
-			christmas: "weihnachten",
-			and: "und",
-			happy: "glücklich",
-			new: "neu",
-			year: "jahr" };
-		oldTranslate.german = function (inputText) {
-		return lexicon[inputText];
-	};
-	return oldTranslate;
-})(Translate);	
+	var lexicon = { merry: "fröhlich",
+                  christmas: "weihnachten",
+                  and: "und",
+                  happy: "glücklich",
+                  new: "neu",
+                  year: "jahr" };
+
+	oldTranslate.german = function (inputText) {
+    inputTextArray = inputText.split(" ");  // convert string into array of individual words
+    var outputString = "";  // initialize blank string
+    inputTextArray.forEach(function(currentWord) {
+      if (lexicon[currentWord]) {  // is currentWord in the lexicon?
+        outputString += lexicon[currentWord] + " ";  // if so, add translation of currentWord to outputString
+      } else {
+        outputString += "??? ";  // if not, add "???" to outputString
+      }
+    });
+    return outputString;
+  };
+
+  return oldTranslate;
+})(Translate);
